92fb5502b4b67542691c552117e1e68b8de6cb38
RB <rbrunson01@gmail.com>
2021-08-29 08:41:11 -0500

    Accept user input

--- Unit_01/CompoundAssignmentOperators/src/Main.java
+++ Unit_01/CompoundAssignmentOperators/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -39,5 +41,42 @@ public class Main {
 //        System.out.printf("x %= 2; x = %f", x);
 
         System.out.println();
+
+        x = GetUserNumber();
+
+        x = x + 1;
+        x = x - 1;
+        x = x * 2;
+        x = x / 2;
+        x = x % 2;
+        System.out.printf("x = %f\n", x);
+
+//        System.out.printf("x++; x = %f", x);
+//        System.out.printf("x--; x = %f", x);
+//        System.out.printf("x *= 2; x = %f", x);
+//        System.out.printf("x /= 2; x = %f", x);
+//        System.out.printf("x %= 2; x = %f", x);
+    }
+
+    private static double GetUserNumber() {
+        double x;
+        System.out.print("Please enter a number: ");
+        Scanner scan = new Scanner(System.in);
+        while(true)
+        {
+            String input = scan.nextLine();
+            try {
+                x = Double.parseDouble(input);
+                break;
+            }
+            catch (Exception e)
+            {
+                System.out.print("That's not a number. Please enter a number: ");
+            }
+        }
+
+        System.out.printf("User value: x = %f\n", x);
+        scan.close();
+        return x;
     }
 }
