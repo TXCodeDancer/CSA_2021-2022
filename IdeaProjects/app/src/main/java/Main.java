67cba9e3ebf6b6cfde0d358222d216deb6be2f5d
RB <rbrunson01@gmail.com>
2021-09-03 22:41:57 -0500

    WIP: Delete app demo

--- IdeaProjects/app/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Hello y'all!");
-        Person person = new Person("Mr. Brunson");
-        System.out.println("Hi " + person.getName() + "!");
-        System.out.println();
-    }
-}
