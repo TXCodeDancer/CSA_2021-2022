262faa09c9f3af543ddeed3f5dc5a16fe1e68c37
RB <rbrunson01@gmail.com>
2021-09-05 09:48:52 -0500

    WIP: Add missing code cleanup

--- Unit_02/CSAJava.02.01.Objects/2.1 Objects - Intances of Classes/Cat Class/src/Main.java
+++ Unit_02/CSAJava.02.01.Objects/2.1 Objects - Intances of Classes/Cat Class/src/Main.java
@@ -1,45 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-   public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-      // Create instance of class with no-argument constructor
-      Cat myCat = new Cat();
-      System.out.println();
-      System.out.println("The default Cat is:");
-      System.out.println(myCat);
+        // Create instance of class with no-argument constructor
+        Cat myCat = new Cat();
+        System.out.println();
+        System.out.println("The default Cat is:");
+        System.out.println(myCat);
 
-      // Create instance of class with argument constructor
-      Cat panda  = new Cat("Panda", 16, "black and white");
-      Cat rusty  = new Cat("Rusty", 15, "orange tabby");
-      Cat ben    = new Cat("Ben", 18, "black and white");
-      Cat bowie  = new Cat("Bowie", 13, "white");
+        // Create instance of class with argument constructor
+        Cat panda = new Cat("Panda", 16, "black and white");
+        Cat rusty = new Cat("Rusty", 15, "orange tabby");
+        Cat ben = new Cat("Ben", 18, "black and white");
+        Cat bowie = new Cat("Bowie", 13, "white");
 
-      // Create a list of my cats
-      List<Cat> myCats = new ArrayList<>();
-      myCats.add(panda);
-      myCats.add(rusty);
-      myCats.add(ben);
-      myCats.add(bowie);
+        // Create a list of my cats
+        List<Cat> myCats = new ArrayList<>();
+        myCats.add(panda);
+        myCats.add(rusty);
+        myCats.add(ben);
+        myCats.add(bowie);
 
-      outputMyCats(myCats);
+        outputMyCats(myCats);
 
-      // Panda and Ben are now sleeping
-      panda.sleeps();
-      ben.sleeps();
+        // Panda and Ben are now sleeping
+        panda.sleeps();
+        ben.sleeps();
 
-      outputMyCats(myCats);
+        outputMyCats(myCats);
 
-   }
+    }
 
-   // Output information about each of my cats
-   private static void outputMyCats(List<Cat> myCats) {
-      System.out.println();
-      System.out.println("My cats are:");
-      for(Object cat : myCats)
-      {
-         System.out.println(cat);
-      }
-   }
+    // Output information about each of my cats
+    private static void outputMyCats(List<Cat> myCats)
+    {
+        System.out.println();
+        System.out.println("My cats are:");
+        for (Object cat : myCats)
+        {
+            System.out.println(cat);
+        }
+    }
 }
