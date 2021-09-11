077985f592f343652fa3a133baed87596b13a2a7
RB <rbrunson01@gmail.com>
2021-09-11 07:26:37 -0500

    WIP: Add Cat Class Implementation task

---
+++ CSAJava.Unit02.UsingObjects/2.1 Objects - Instances of Classes/Cat Class Implementation/test/Tests.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+  @Test
+  public void testDefault() {
+    // put your test here
+    Cat cat = new Cat();
+    String actual = cat.toString();
+    String expected = ", the -2147483648-year-old  cat, is not sleeping.";
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testParameterConstructor() {
+    // put your test here
+    Cat cat = new Cat("Bowie", 13, "white");
+    String actual = cat.toString();
+    String expected = "Bowie, the 13-year-old white cat, is not sleeping.";
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testIsSleeping() {
+    // put your test here
+    Cat cat = new Cat("Bowie", 13, "white");
+    cat.sleeps();
+    String actual = cat.toString();
+    String expected = "Bowie, the 13-year-old white cat, is sleeping.";
+    assertEquals(expected, actual);
+  }
+}
