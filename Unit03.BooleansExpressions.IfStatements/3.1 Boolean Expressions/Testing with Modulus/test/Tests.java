4cfa140f99c34317d1a5983ad237fc279b69f7a5
RB <rbrunson01@gmail.com>
2021-09-25 14:15:47 -0500

    WIP: Testing with Modulus tests pass

--- Unit03.BooleansExpressions.IfStatements/3.1 Boolean Expressions/Testing with Modulus/test/Tests.java
+++ Unit03.BooleansExpressions.IfStatements/3.1 Boolean Expressions/Testing with Modulus/test/Tests.java
@@ -4,7 +4,23 @@ import org.junit.Test;
 public class Tests {
   @Test
   public void testSolution() {
-    // put your test here
-    Assert.fail("Tests not implemented for the task");
+   Assert.assertTrue(BooleanExpressions.isPositive(1));
+    Assert.assertFalse(BooleanExpressions.isPositive(0));
+    Assert.assertFalse(BooleanExpressions.isPositive(-1));
+
+    Assert.assertFalse(BooleanExpressions.isNegative(1));
+    Assert.assertFalse(BooleanExpressions.isNegative(0));
+    Assert.assertTrue(BooleanExpressions.isNegative(-1));
+
+    Assert.assertTrue(BooleanExpressions.isEven(0));
+    Assert.assertFalse(BooleanExpressions.isEven(1));
+    Assert.assertTrue(BooleanExpressions.isEven(2));
+
+    Assert.assertFalse(BooleanExpressions.isOdd(0));
+    Assert.assertTrue(BooleanExpressions.isOdd(1));
+    Assert.assertFalse(BooleanExpressions.isOdd(2));
+
+    Assert.assertTrue(BooleanExpressions.isMultipleOf(4, 2));
+    Assert.assertFalse(BooleanExpressions.isMultipleOf(5, 2));
   }
 }
