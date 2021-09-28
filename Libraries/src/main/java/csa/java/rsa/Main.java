2f7a6e433956715415c63ddbf8ba29a3c32022d4
RB <rbrunson01@gmail.com>
2021-09-27 23:11:36 -0500

    WIP: RSA encrypt basic ASCII set as test

--- Libraries/src/main/java/csa/java/rsa/Main.java
+++ Libraries/src/main/java/csa/java/rsa/Main.java
@@ -5,6 +5,9 @@ package csa.java.rsa;
  ** @created 9/26/2021
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] args)
@@ -38,10 +41,10 @@ public class Main
 
         ///////////////////////////////////
         System.out.println();
-        String planText = "This is a test!";
-        System.out.println(planText);
+        String plainText = "This is a test!";
+        System.out.println(plainText);
 
-        encodedMessage = rsa.encode(planText);
+        encodedMessage = rsa.encode(plainText);
 
         for(int e : encodedMessage)
         {
@@ -52,8 +55,40 @@ public class Main
         }
         System.out.println();
 
-        planText = rsa.decodeToString(encodedMessage);
-        System.out.println(planText);
+        plainText = rsa.decodeToString(encodedMessage);
+        System.out.println(plainText);
+        System.out.println();
+
+        ///////////////////////////////////
+        System.out.println();
+        List<Character> characters = new ArrayList<Character>();
+        for(char ch = 0; ch < 128; ch++)
+        {
+            characters.add(ch);
+        }
+
+        char[] chars = new char[characters.size()];
+        for(int i = 0; i < chars.length; i++)
+        {
+            chars[i] = characters.get(i);
+        }
+
+        plainText = new String(chars);
+        System.out.println(plainText);
+
+        encodedMessage = rsa.encode(plainText);
+
+        for(int e : encodedMessage)
+        {
+            System.out.print(String.format("%02X", e));
+            String h = Integer.toHexString(e);
+            int l = Integer.parseInt(h, 16);
+//            System.out.print(l);
+        }
+        System.out.println();
+
+        plainText = rsa.decodeToString(encodedMessage);
+        System.out.println(plainText);
         System.out.println();
     }
 }
