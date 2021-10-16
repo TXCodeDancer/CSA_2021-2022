711c3eaf0c8b8e8a250152d5dece16e6b41897af
RB <rbrunson01@gmail.com>
2021-10-16 16:58:05 -0500

    WIP: Add Private Key Main

--- Projects/RSAEncryption/RSA Basics/Private Key/src/Main.java
+++ Projects/RSAEncryption/RSA Basics/Private Key/src/Main.java
@@ -6,4 +6,14 @@
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        System.out.println();
+        PublicKey publicKey = new PublicKey();
+        System.out.println("Public key <" + publicKey.getN() + ", " + publicKey.getE() + ">");
+
+        PrivateKey privateKey = new PrivateKey(publicKey.getN(), publicKey.getPhi_n(), publicKey.getE());
+        System.out.println("Private key <" + privateKey.getN() + ", " + privateKey.getD() + ">");
+        System.out.println();
+    }
 }
