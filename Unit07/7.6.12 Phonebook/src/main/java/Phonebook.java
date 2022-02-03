984cbd4e28c5f879ad4230fc1111d336d658f76e
RB <rbrunson01@gmail.com>
2022-02-03 16:13:15 -0600

    WIP: Add 7.6.12 Phonebook starter

---
+++ Unit07/7.6.12 Phonebook/src/main/java/Phonebook.java
@@ -0,0 +1,15 @@
+
+/*
+ ** @author Rick Brunson, Team Brunson Software, LLC
+ ** @created 2/3/2022
+ */
+
+import java.util.ArrayList;
+
+public class Phonebook
+{
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+
+}
+
