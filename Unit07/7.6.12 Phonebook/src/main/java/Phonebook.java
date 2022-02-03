67973afaca1c064934edef22ecf4092522e959e7
RB <rbrunson01@gmail.com>
2022-02-03 17:00:22 -0600

    WIP: Phonebook passes

--- Unit07/7.6.12 Phonebook/src/main/java/Phonebook.java
+++ Unit07/7.6.12 Phonebook/src/main/java/Phonebook.java
@@ -10,6 +10,35 @@ public class Phonebook
 {
     private ArrayList<Contact> contacts = new ArrayList<Contact>();
 
+    public void addContact(Contact contact)
+    {
+        sortAdd(contact);
+    }
+
+    private void sortAdd(Contact contact)
+    {
+
+        for(int i = contacts.size() - 1; i >= 0 ; i--)
+        {
+            String name = contacts.get(i).getName();
+
+            if(contact.getName().compareTo(name) > 0)
+            {
+                contacts.add(i + 1, contact);
+                return;
+            }
+        }
+        contacts.add(0, contact);
+    }
+
+    public void printPhonebook()
+    {
+        for(Contact contact: contacts)
+        {
+            System.out.println(contact.getName());
+        }
+    }
+
 
 }
 
